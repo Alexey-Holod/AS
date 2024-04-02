0345d6e5ad9300d7b08033a07ece8b8e09f85696
AlexeyHolod <Alexey95holod@yandex.ru>
2024-04-02 23:55:10 +0500

    Первая стартовая траница
    Стартовая стрница поставлена на сервер и отдается по запросу.
    Отчищена от мусора. Частично переведена на ру.

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from  .views import *
 
 urlpatterns = [
-    path('', ),
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
 ]
