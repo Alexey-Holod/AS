fa181cb600f1654f474ee24843d51c8e6ffbacc4
AlexHDD <Alexey95holod@yandex.ru>
2025-02-21 22:58:18 +0500

    Сделана кнопка "Заказать"

--- delivery/urls.py
+++ delivery/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('add_to_cart/<int:id_product>/<int:id_size>', add_to_cart, name='add_to_cart'),
+    path('order/<int:id_product>/<slug:id_size>', order, name='order'),
 ]
--- delivery/views.py
+++ delivery/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 import datetime
 from home.models import Product, Size
+from home.views import product_details
 from django.contrib.auth.models import User
 from .models import ProductDelivery, DeliveryStatus
 
@@ -24,3 +25,18 @@ def add_to_cart(request, id_product, id_size):
 
     P_D.save()
     return redirect('home')
+
+def order(request, id_product, id_size, ):
+    # Получаем объект размера что-бы использовать его при поиске
+    size = Size.objects.get(size_range=id_size)
+    # Делаю поиск в таблице ProductDelivery с помощью двух параметров
+    ProdDeliVOrder = ProductDelivery.objects.filter(ProductID = id_product).filter(ProductSize = size)
+    # Т.к. при запросе выше мы получили обект "Набор", то используя его запросим один конкретный объект
+    Here = ProductDelivery.objects.get(id = ProdDeliVOrder[0].id)
+    # Далее запросим объект статуса заказа который нам нужен "ЗАКАЗАН"
+    DevStatus = DeliveryStatus.objects.get(id = 2)
+    # Теперь присваиваем свойству объекта заказа новый статус
+    Here.Name_product = DevStatus
+    # Сохраняем
+    Here.save()
+    return product_details(request, id_product, id_size, 'disable_size')
