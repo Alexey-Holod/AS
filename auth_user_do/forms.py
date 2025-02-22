c5eb9c91d0b57ff5e4583976c0b9ec2eaed3d190
AlexHDD <Alexey95holod@yandex.ru>
2025-02-23 02:01:29 +0500

    Добавление номера в профиль пользователя.

--- auth_user_do/forms.py
+++ auth_user_do/forms.py
@@ -4,11 +4,12 @@ from django.contrib.auth.models import User
 
 
 class AuthRegUserForm(UserCreationForm):
-    first_name = forms.CharField(label='Имя', widget=forms.TextInput(attrs={'class': 'form-inputp', 'placeholder': "Имя"}))
-    username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-inputp', 'placeholder': "Логин"}))
-    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-inputp', 'placeholder': "Пароль"}))
-    password2 = forms.CharField(label='Повторите пароль', widget=forms.PasswordInput(attrs={'class': 'form-inputp', 'placeholder': "Повторите пароль"}))
-    email = forms.CharField(label='Email', widget=forms.EmailInput(attrs={'class': 'form-inputp', 'placeholder': "Email"}))
+    first_name = forms.CharField(label='Имя', widget=forms.TextInput(attrs={'class': 'form-input', 'placeholder': "Имя"}))
+    username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-input', 'placeholder': "Логин"}))
+    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input', 'placeholder': "Пароль"}))
+    password2 = forms.CharField(label='Повторите пароль', widget=forms.PasswordInput(attrs={'class': 'form-input', 'placeholder': "Повторите пароль"}))
+    email = forms.CharField(label='Email', widget=forms.EmailInput(attrs={'class': 'form-input', 'placeholder': "Email"}))
+    phone = forms.IntegerField(label='Телефон', widget=forms.NumberInput(attrs={'class': 'form-inputp', 'placeholder': "Телефон"}))
 
     class Meta:
         model = User
@@ -19,4 +20,5 @@ class AuthRegUserForm(UserCreationForm):
             'password1': forms.PasswordInput(attrs={'class': 'form-authp'}),
             'password2': forms.PasswordInput(attrs={'class': 'form-authp'}),
             'email': forms.EmailInput(attrs={'class': 'form-authp'}),
+            'phone': forms.NumberInput(attrs={'class': 'form-authp'}),
         }
--- auth_user_do/views.py
+++ auth_user_do/views.py
@@ -2,6 +2,8 @@ from django.views import View
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 from .forms import AuthRegUserForm
+from django.contrib.auth.models import User
+from .models import Profile
 
 class Reg(View):
     template_name = 'auth_user_do/reg.html'
@@ -17,11 +19,22 @@ class Reg(View):
 
         if form.is_valid():
             form.save()
+            # UserPhone = User.objects.get(id=request.user)
             first_name = form.cleaned_data.get('first_name')
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password1')
             user = authenticate(username=username, first_name=first_name, password=password)
             login(request, user)
+
+            # -----------------------------------------
+
+            UserPhone = Profile.objects.get(user = request.user.id)
+            UserPhone.Phone = form.cleaned_data.get('phone')
+            UserPhone.save()
+            print('ХУЙЙЙЙЙ', UserPhone.Phone)
+
+            # -----------------------------------------
+
             return redirect('/')
         else:
             context = {
