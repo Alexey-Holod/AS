3d076d5df9e87afbf9ed3ab8d4e7b9f97e5d4423
Alexey-Holod <Alexey95holod@yandex.ru>
2025-09-23 17:59:23 +0500

    Продолжил делать сброс пароля
    Ссылка на статью: https://fixmypc.ru/post/sozdanie-formy-vosstanovleniia-parolia-v-django/?ysclid=mfwhucge7x206868691#kak-realizuetsia-protsess-vosstanovleniia-parolia

--- auth_user_do/urls.py
+++ auth_user_do/urls.py
@@ -1,9 +1,19 @@
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
 from .views import *
-from django.urls import path
+from django.urls import path, include
+
+# Сыылка по которо делался сброс пароля: https://fixmypc.ru/post/sozdanie-formy-vosstanovleniia-parolia-v-django/?ysclid=mfwhucge7x206868691#kak-realizuetsia-protsess-vosstanovleniia-parolia
 
 urlpatterns = [
     path('in/', LoginView.as_view(template_name='auth_user_do/auth_in.html'), name='auth_user_do'),
     path('out/', LogoutView.as_view(), name='logout'),
-    path('reg/', Reg.as_view(), name='registration')
+    path('reg/', Reg.as_view(), name='registration'),
+    path('reset_password/', PasswordResetView.as_view(),
+         name ='reset_password'),
+    path('reset_password_sent/', PasswordResetDoneView.as_view(),
+         name ='password_reset_done'),
+    path('reset/<uidb64>/<token>', PasswordResetConfirmView.as_view(),
+          name ='password_reset_confirm'),
+    path('reset_password_complete/', PasswordResetCompleteView.as_view(),
+         name ='password_reset_complete'),
 ]
